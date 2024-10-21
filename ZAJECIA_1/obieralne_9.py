4998ea2531592322565179c7800de75ad0663119
MZAWI <mzawieracz@student.agh.edu.pl>
2024-10-21 22:14:58 +0000

    dodanie funkcji cache

--- ZAJECIA_1/obieralne_9.py
+++ ZAJECIA_1/obieralne_9.py
@@ -1,4 +1,4 @@
-# Funkcja znajdująca dzielniki liczby
+#Funkcja znajdująca dzielniki liczby
 def div_sum(number):
     div = [1] 
     mid = int(number**(1/2)) + 1
@@ -10,16 +10,36 @@ def div_sum(number):
                 div.append(n2)
     return sum(div)
 
+
 seek = 10**6 # liczba do której szukamy zaprzyjaźnionych
+
+div_sums = []
+for i in range(seek):
+    div_sums.append(div_sum(i))
+
 
 result = []
 for n1 in range(seek):
-    n2 = div_sum(n1)
-    if div_sum(n2) == n1 and n1 != n2:
+    n2 = div_sums[n1]
+    if n2 > seek:
+        continue
+    if div_sums[n2] == n1 and n1 != n2:
         # print(f"{n1} i {n2}")
-        result.append(f"{n1} i {n2}")
+        result.append(f"{n2} i {n1}")
 
 # Usuwanie powtórzeń
 for i in range(len(result)):
-    if i % 2 == 0:
+    if i % 2 == 1:
         print(result[i])
+
+# Measure-Command {py obieralne_9.py}
+# Hours             : 0
+# Minutes           : 0
+# Seconds           : 30
+# Milliseconds      : 56
+# Ticks             : 300568144
+# TotalDays         : 0,000347879796296296
+# TotalHours        : 0,00834911511111111
+# TotalMinutes      : 0,500946906666667
+# TotalSeconds      : 30,0568144
+# TotalMilliseconds : 30056,8144
