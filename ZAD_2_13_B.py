31b12c11971693e044cfa30dc11a108e8ae74e17
MZAWI <mzawieracz@student.agh.edu.pl>
2024-11-05 15:56:44 +0100

    Add files via upload

--- ZAD_2_13_B.py
+++ ZAD_2_13_B.py
@@ -1,44 +1,38 @@
 import random
 
-def make_dict(list1,list2): # Tworzenie słownika z dwóch list
-    dict = {}
-    for element in list1:
-            value = list1.index(element)
-            dict.update({element : list2[value]})
-    return dict
+def make_dict(keys,values): # Tworzenie słownika z dwóch list
+    return dict(zip(keys,values))
     
-def make_rand_dict(list1,list2): # Tworzenie słownika losowego oraz listy umożliwiającej złozenie słownika z powrotem w listę
-    dict = {}
-    for element in list1:
-        index = list1.index(element)
+def make_rand_dict(list1,list2): # Tworzenie słownika losowego 
+    dictionary = {}
+    for i in range(len(list1)):
         if random.choice([True,False]):
-            dict.update({element : list2[index]})
+            dictionary[keys[i]] = values[i]
         else:
-            dict.update({list2[index] : element})
-    return dict
+            dictionary[values[i]] = keys[i]
+    return dictionary
 
 def dict_to_lists(dict): # Tworzenie list na podstawie dziennika
-    list1 = list(dict.keys())
-    list2 = list(dict.values())
-    return list1, list2
+    keys = list(dict.keys())
+    values = list(dict.values())
+    return keys, values
 
 
-a = ["a", "b" ,"c"]
-b = ["apple", "banana", "cherry"]
+keys = ["a", "b" ,"c", "d"]
+values = ["apple", "banana", "cherry", "dinosaur"]
 
-if len(a) != len(b):
+if len(keys) != len(values):
     print("Błąd: Listy powinny mieć taką samą długość")
 else:
-    mode = int(input(" 1 - Tryb scalania list\n 2 - Tryb losowego scalania list\n Wybierz tryb: "))
+    mode = int(input(" 1 - Tryb scalania list\n 2 - Tryb losowego scalania list\nWybierz tryb: "))
     if mode == 1:
-        dictionary = make_dict(a,b)
+        dictionary = make_dict(keys, values)
     elif mode == 2:
-        dictionary = make_rand_dict(a,b)
-        print(dictionary)
+        dictionary = make_rand_dict(keys, values)
         
-    print(f"Słownik:\n{dictionary}\n")
+    print(f"Słownik: {dictionary}\n")
     list1, list2 = dict_to_lists(dictionary)
-    print("Listy:")
-    print(list1)
-    print(list2)
+
+    print("Klucze: ", list1)
+    print("Watości: ", list2)
 
