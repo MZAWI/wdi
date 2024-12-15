d7f83cd26f479b34a7f7a75cafe14a364c8c4140
MZAWI <mzawieracz@student.agh.edu.pl>
2024-12-15 17:22:42 +0100

    added regular polygon recognition

--- ZAJECIA_5/ZAD_5_15.py
+++ ZAJECIA_5/ZAD_5_15.py
@@ -1,7 +1,8 @@
 # Class implementation - Remake
 # Point format on x,y axis: (x,y)
 
-from math import sqrt
+import itertools
+import math
 
 class Point:
     def __init__(self, coords):
@@ -17,7 +18,7 @@ class Point:
     def distance(self, other_point): # Calculate distance between two points
         dx = self.x - other_point.x
         dy = self.y - other_point.y
-        return sqrt(dx**2 + dy**2)
+        return math.sqrt(dx**2 + dy**2)
     
     def __eq__(self, other_point): # Define equality
         if isinstance(other_point, Point):
@@ -26,27 +27,68 @@ class Point:
 
     # Return list of closest points of the same distance
     # Excluding comparision point
-    def closest(self, point_list: list):
+    def closest(self, point_list: list, tolerance = 1e-9):
         point_list = [point for point in point_list if point != self] # Remove self
         distances = [(point, self.distance(point)) for point in point_list]
         min_distance = min(distances, key = lambda x: x[1])[1]
-        closest = [point for point, dist in distances if dist == min_distance]
-        return closest
+        return [point for point, dist in distances if abs(dist - min_distance) <= tolerance]
+        
+    def angle(self, point1, point2): # Find angle between points, where self is middle
+        u = Point((point1.x - self.x, point1.y - self.y))
+        v = Point((point2.x - self.x, point2.y - self.y))
+        dot_product = (u.x * v.x + u.y * v.y)
+        u_magnitude = math.sqrt((u.x**2)+(u.y)**2)
+        v_magnitude = math.sqrt((v.x**2)+(v.y)**2)
+
+        cos_angle = dot_product / (u_magnitude * v_magnitude)
+        return math.acos(cos_angle)
     
-    # Check if points from a list can draw equilateral rectangle
-def is_equilateral_rectangle(point_list: list):
-    if not all(isinstance(point, Point) for point in point_list):
+    # Check if point is inside of a polygon (Ray-casting algorithm)
+    def is_inside_of_polygon(self, point_list: list):
+        pass
+
+# Check if points from a list can draw equilateral polygon
+def is_equilateral_polygon(point_list: list):
+    if not all(isinstance(point, Point) for point in point_list) or len(point_list) < 3:
         return False
-    if all([len(point.closest(point_list)) == 2 for point in point_list]):
+    if all([len(point.closest(point_list)) == 2 for point in point_list]):        
         return True
     return False
 
-def main():
-    point_list = [(0,0),(1,1),(1,0),(0,1)]
+# Check if all inside angles are the same 
+def same_angles(point_list: list):
+    angles = []
+    for point in point_list:
+        close = point.closest(point_list)
+        angles.append(point.angle(close[0],close[1]))
+    angles = [round(angle,9) for angle in angles]
+    if all(angle == angles[0] for angle in angles):
+        return True
+    return False
+
+def is_Regular_Polygon(point_list):
     point_list = [Point(point) for point in point_list]
+    if is_equilateral_polygon(point_list) and same_angles(point_list):
+        return True
+    return False
 
 
-    print(is_equilateral_rectangle(point_list))
+def find_regular_polygon(point_list):
+    combo = []
+    for i in range(3, len(point_list)+1):
+        combo = itertools.combinations(point_list, i)
+        for figure in combo:
+            # print(figure)
+            if is_Regular_Polygon(figure):
+                return figure
+
+def main():
+    a = math.sqrt(3)/2
+    # point_list = [(0,0),(1,0),(0,1),(1,1)]
+    point_list = [(0,0),(1,0),(0.5,a)]
+    print(is_Regular_Polygon(point_list))
+    print(find_regular_polygon(point_list))
+    Point((0.5,0.2)).is_inside_of_polygon(point_list)
 
 if __name__ == "__main__":
     main()
