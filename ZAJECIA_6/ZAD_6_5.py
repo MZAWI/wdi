73c9cf47a7877e921058916e490b999f54a2ad68
MZAWI <mzawieracz@student.agh.edu.pl>
2024-12-17 16:00:26 +0100

    zad_6

--- ZAJECIA_6/ZAD_6_5.py
+++ ZAJECIA_6/ZAD_6_5.py
@@ -7,7 +7,7 @@ def nested_list(depth = 5, max_elements = 3,  max_value = 5):
     if depth == 0: # stop nesting
         return random.randint(0,max_value)
     for _ in range(random.randint(1,max_elements)):
-        nest_list.append(nested_list(depth-1))
+        nest_list.append(nested_list(depth-1,max_elements, max_value))
     return nest_list
 
 def nested_sum(nested_list: list):
@@ -21,7 +21,7 @@ def nested_sum(nested_list: list):
 
 
 def main():
-    example = nested_list(3,2,1)
+    example = nested_list(3,2,5)
     print(example)
     print(nested_sum(example))
 
